280d4748f3c15ac19ad48bdb49f5209da280ecdb
Alicia Evans <alicia.adams.evans@gmail.com>
2018-10-14 09:30:56 -0700

    Implemented scope for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,6 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails") }
+  scope :react_portfolio_items, -> { where(subtitle: "React") }
 end
