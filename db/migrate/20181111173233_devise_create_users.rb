b9c12e659142b30403e0c165ae1cbd3ffaa8577f
Alicia Evans <unleash.alicia@gmail.com>
2019-02-03 16:37:55 -0800

    Implements basic auth

--- db/migrate/20181111173233_devise_create_users.rb
+++ db/migrate/20181111173233_devise_create_users.rb
@@ -7,6 +7,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      ## Custom Fields
+      t.string :name
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
